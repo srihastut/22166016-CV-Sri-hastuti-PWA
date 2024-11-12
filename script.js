9f0451a985fa0af0084cc6b40bf83df3c9e01e99
srihastut <149176809+srihastut@users.noreply.github.com>
2024-11-12 11:23:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ window.onscroll = () => {
 
 // Pendaftaran Service Worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
+  navigator.serviceWorker.register('/https://sri-hastuti-pwa.vercel.app/service-worker.js')
     .then(registration => {
       console.log('Service Worker registered with scope:', registration.scope);
 
